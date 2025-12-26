d4fcd910a5e2843c164439660d371b2ea9543820
Sergey <sergeys1095@gmail.com>
2025-12-26 18:55:25 +0300

    Begin add methods

--- Main.py
+++ Main.py
@@ -1,10 +1,37 @@
 import tkinter
-from tkinter import messagebox
+import json
+import random
+import os
+import sys
+from PIL import Image, ImageTk
+from tkinter import messagebox, ttk, font
 
-class Quize:
+class Quiz:
     def __init__(self):
-        self.root = tkinter.Tk()
-        self.root.title("Квиз")
-        self.root.geometry("700x700")
-        self.score = 0
-        self.current_question = 0
+        try:
+            self.root = tkinter.Tk()
+            self.window()
+            self.questions()
+            self.variables()
+            self.styles()
+            self.widgets()
+            self.new_game()
+
+        except Exception as e:
+            self.show_critical_error(f"Ошибка при запуске игры:\n{str(e)}")
+
+    def window(self):
+        self.root.title("Умный Квиз - Проверь свои знания!")
+        self.root.geometry("1000x750")
+        self.root.minsize(800, 600)
+
+        try:
+            self.root.iconbitmap(default="icon.ico")
+        except:
+            pass
+
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def questions(self):
+        self.questions_file = "Questions.json"
+        self.all_questions = []
